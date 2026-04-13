8e081b883f2a7e4dda1ae159ecac666e36761370
rogelio <rogelio.ortigosa@upct.es>
2026-04-13 21:43:19 +0200

    --

--- src/Solvers/LineSearches.jl
+++ src/Solvers/LineSearches.jl
@@ -117,7 +117,7 @@ function InjectivityCheck(α, ∇u, ∇du, β)
   F = ∇u + one(∇u)
   J = det(F)
   H = J * inv(F)'
-  println("Jacobian print")
+  #println("Jacobian print")
    if det(F+∇du) < 0.5
    @show det(F), det(F+∇du)
    end
