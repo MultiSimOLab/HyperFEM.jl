34814a507acba08e4a6cb0c38e7d71d43cc6f6a0
rogelio <rogelio.ortigosa@upct.es>
2026-04-13 16:41:32 +0200

    --

--- src/Solvers/LineSearches.jl
+++ src/Solvers/LineSearches.jl
@@ -87,21 +87,22 @@ struct Injectivity_Preserving_LS{A} <: AbstractLineSearch
     xh = FEFunction(U, x)
     dxh = FEFunction(V, dx)
     α = update_cellstate!(obj, xh, dxh)
+    @show α
     m = 0
-    R₀ = sum(abs(b[r]' * dx[r]) for r in ranges)
-    #println("R₀")
-    #@show b[ranges[1]]' * dx[ranges[1]], b[ranges[2]]' * dx[ranges[2]]
-    while α > αmin && m < maxiter
-      residual!(b, op, x + α * dx)
-      R = sum(abs(b[r]' * dx[r]) for r in ranges)
-      #println("R")
-      #@show b[ranges[1]]' * dx[ranges[1]], b[ranges[2]]' * dx[ranges[2]]
-      if abs(R) <= abs(c * R₀)
-        break
-      end
-      α *= ρ
-      m += 1
-    end
+    # R₀ = sum(abs(b[r]' * dx[r]) for r in ranges)
+    # #println("R₀")
+    # #@show b[ranges[1]]' * dx[ranges[1]], b[ranges[2]]' * dx[ranges[2]]
+    # while α > αmin && m < maxiter
+    #   residual!(b, op, x + α * dx)
+    #   R = sum(abs(b[r]' * dx[r]) for r in ranges)
+    #   #println("R")
+    #   #@show b[ranges[1]]' * dx[ranges[1]], b[ranges[2]]' * dx[ranges[2]]
+    #   if abs(R) <= abs(c * R₀)
+    #     break
+    #   end
+    #   α *= ρ
+    #   m += 1
+    # end
     return α
   end
 
