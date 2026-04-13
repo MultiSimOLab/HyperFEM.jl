6c8af9e90c98e4950adeed9b25518299e02c881c
rogelio <rogelio.ortigosa@upct.es>
2026-04-13 16:48:12 +0200

    --

--- src/Solvers/LineSearches.jl
+++ src/Solvers/LineSearches.jl
@@ -89,11 +89,11 @@ struct Injectivity_Preserving_LS{A} <: AbstractLineSearch
     α = update_cellstate!(obj, xh, dxh)
     @show α
     m = 0
-    # R₀ = sum(abs(b[r]' * dx[r]) for r in ranges)
+    R₀ = sum(abs(b[r]' * dx[r]) for r in ranges)
     # #println("R₀")
     # #@show b[ranges[1]]' * dx[ranges[1]], b[ranges[2]]' * dx[ranges[2]]
     # while α > αmin && m < maxiter
-    #   residual!(b, op, x + α * dx)
+       residual!(b, op, x + α * dx)
     #   R = sum(abs(b[r]' * dx[r]) for r in ranges)
     #   #println("R")
     #   #@show b[ranges[1]]' * dx[ranges[1]], b[ranges[2]]' * dx[ranges[2]]
