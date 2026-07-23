2c4c3b6d9e73e83a43c1568fe1e0b9f18336d108
Miguel Masó <miguel.maso@upc.edu>
2026-07-23 19:13:48 +0200

    fixed wrong type specification

--- src/PhysicalModels/ViscousModels.jl
+++ src/PhysicalModels/ViscousModels.jl
@@ -23,7 +23,7 @@ function (obj::ViscousIncompressible)()
   return Ψ, ∂Ψ∂F, ∂Ψ∂F∂F
 end
 
-function update_time_step!(obj<:Visco, Δt::Float64)
+function update_time_step!(obj::Visco, Δt::Float64)
   obj.Δt[] = Δt
 end
 
@@ -32,7 +32,7 @@ function Gridap.CellData.CellState(::Visco, F0::TensorValue, points::Measure)
   CellState(v, points)
 end
 
-function Gridap.CellData.CellState(obj<:Visco, points::Measure)
+function Gridap.CellData.CellState(obj::Visco, points::Measure)
   CellState(obj, I3, points)
 end
 
@@ -41,13 +41,13 @@ function initialize_state(obj::ViscousIncompressible, points::Measure)
   CellState(obj, points)
 end
 
-function update_state!(obj<:Visco, state, F, Fn)
+function update_state!(obj::Visco, state, F, Fn)
   _, Se, ∂Se∂Ce = SecondPiola(obj.elasto)
   return_mapping(A, F, Fn) = ReturnMapping(obj, Se, ∂Se∂Ce, F, Fn, A)
   update_state!(return_mapping, state, F, Fn)
 end
 
-function Dissipation(obj<:Visco)
+function Dissipation(obj::Visco)
   _, Se, ∂Se∂Ce = SecondPiola(obj.elasto)
   D(F, Fn, A) = ViscousDissipation(obj, Se, ∂Se∂Ce, F, Fn, A)
 end
