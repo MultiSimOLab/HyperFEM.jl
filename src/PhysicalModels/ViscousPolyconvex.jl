b6f2c6c9845475199603b40a9baba93ae381ccb7
Miguel Masó <miguel.maso@upc.edu>
2026-07-30 14:53:01 +0200

    added algebra benchmark

--- src/PhysicalModels/ViscousPolyconvex.jl
+++ src/PhysicalModels/ViscousPolyconvex.jl
@@ -118,9 +118,6 @@ function ∂Cv⁻¹∂C(obj::ViscousPolyconvex, C, Cn, Cvn)
   IIIb = det(B)
   M = invC * invB * invC
   -Τ * IIIb^(-1/3) * (IIsym(invC) - (1/3) * (B ⊗ M))
-  # ∂invCv∂B = det(B)^(-1/3) * (IIsym(I3) - (1/3) * (B ⊗ inv(B)))
-  # ∂B∂C = -Τ * IIsym(invC)
-  # ∂invCv∂B · ∂B∂C
 end
 
 function return_mapping(obj::ViscousPolyconvex, C, Cn, Cvn)
